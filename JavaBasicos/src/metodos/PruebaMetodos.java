20515062379b53ff18ec2ec37896a6a58cf9fbaa
Nayelli Gutierrez <nayelli.gutierrez.perez@gmail.com>
2022-02-03 22:37:52 -0600

    Ejercicio Java

---
+++ JavaBasicos/src/metodos/PruebaMetodos.java
@@ -0,0 +1,36 @@
+package metodos;
+
+public class PruebaMetodos {
+	public static void main(String[] args) {
+		//metodo estatico
+		//no necesita de un objeto para ser invocado
+		Metodos.imprimirNombre("Pancho");
+		//Metodos.suma(); necesita de un objeto para ser invoados
+		
+		/*
+		//Crear un nuevo objeto que va a contener los métodos y atríbutos 
+		//llamado para traer la clase
+		Metodos metodoPrueba = new Metodos(); //creamos un objeto
+		
+		//Metodo void
+		metodoPrueba.a = 10;
+		metodoPrueba.b = 26;
+		
+		metodoPrueba.sumar(); //invocamos el método
+		
+		//Método Con Retorno
+		//metodoPrueba.sumaConRetorno();//metodo con retorno
+		//System.out.println(metodoPrueba.sumaConRetorno());
+		
+		//este tipo de métodos, se pueden almacenar dentro de una variable
+		int resultadoSuma = metodoPrueba.sumaConRetorno();
+		System.out.println(resultadoSuma - 10);//metodo con retorno
+		
+		//metodo con argumentos 
+		double multiplicacion = metodoPrueba.multiplicarConArgumentos(40, 20.5);
+		System.out.println("El resultado de multiplicacion es: " + multiplicacion);
+		*/
+		
+	}
+
+}
